a1711e0fac394468a885221598cf8b80d76c1de9
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-12-25 18:07:00 -0800

    Init
    Init commit.

---
+++ _src/Chapter 5/HelloWorld/src/com/example/helloworld/HelloWorldMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.view.Menu;
+import org.apache.cordova.*;
+
+public class HelloWorldMainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/chapter5_advanced.html");
+    }  
+}
